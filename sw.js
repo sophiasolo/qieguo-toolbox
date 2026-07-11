af27d8805abf8fbbe219b207fc1a4330dcfe87b9
chuanbo <chuanbo@192.168.2.7>
2026-07-11 18:37:29 +0800

    fix: SW v7+skipWaiting+clientsClaim,安卓强制刷新

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 // 切果NOW文案助手 · Service Worker
-const CACHE = 'qieguo-copy-v6';
+const CACHE = 'qieguo-copy-v7';
 const FILES = ['index.html', 'manifest.json', 'app-icon.png', 'app-icon-ios.png'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
@@ -11,7 +12,7 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
+    )).then(() => self.clients.claim())
   );
 });
 
